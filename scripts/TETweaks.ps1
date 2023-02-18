79a3aba28674741b8959cf90434863149655db20
Silkylynx <125708012+Silkylynx@users.noreply.github.com>
2023-02-17 20:31:20 -0800

    Update TETweaks.ps1

--- scripts/TETweaks.ps1
+++ scripts/TETweaks.ps1
@@ -1,15 +1,15 @@
-       reg add "HKLM\System\CurrentControlSet\Services\WpnUserService" /v "Start" /t REG_DWORD /d "4" /f
+       #reg add "HKLM\System\CurrentControlSet\Services\WpnUserService" /v "Start" /t REG_DWORD /d "4" /f
        #disables windows push notifications
        New-ItemProperty -Path "HKCU:\Software\Microsoft\GameBar\" -Name "AllowAutoGameMode" -Force -Value "1" -PropertyType "DWORD"
        #I think this enables gamemode. #test for restart. 
-       New-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0 -Force
-       #Disables UAC. Test for restart.
-       New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force 
-       New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -PropertyType "DWord" -Value 1 -Force 
-       New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -PropertyType "DWord" -Value 0 -Force 
+       New-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 4 -Force
+       #UAC prompts appear without dimming the screen. 
+       #New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows" -Name "Explorer" -force 
+       #New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -PropertyType "DWord" -Value 1 -Force 
+       #New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -PropertyType "DWord" -Value 0 -Force 
        #Unsure how to undo this one, also disables windows push notifications changing values was not immediately effective. Test for restart.
        #thanks to Andy McRae @ stackoverflow#>
-       New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device" -Name "DevicePasswordLessBuildVersion" -PropertyType "DWord" -Value 2 -Force
+       #New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device" -Name "DevicePasswordLessBuildVersion" -PropertyType "DWord" -Value 2 -Force
        #Forces Passwordless sign in, thanks to Shawn Brink at tenforums
        powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
        #high performance powerplan, thanks to Sven Eberth @ stack overflow
